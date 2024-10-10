b290e5b2c3121af7b61fb1baaaf7de8caa4d2dc9
Jeff Registre <jeffreyregistre@gmail.com>
2024-10-10 02:36:32 -0400

    Basic animation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,22 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
-    [SerializeField]
-    private float stoppingDistance;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float stoppingDistance;
     
     private Vector3 _targetPosition;
     
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     private void Update()
     {
@@ -27,9 +18,10 @@ public class Unit : MonoBehaviour
             transform.position += moveDirection * (moveSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonUp(MouseButton.Left.GetHashCode()))
         {
-            Move(new Vector3(4, 0, 4));
+            var position = MouseWorld.GetMouseWorldPosition();
+            Move(position);
         }
     }
 
